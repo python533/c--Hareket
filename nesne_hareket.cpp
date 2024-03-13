9de8c5bf1b6142bc61d4e3e560a7be3a1e1efaa3
python533 <143389231+python533@users.noreply.github.com>
2024-03-13 15:46:46 +0300

    Movement c++
    c++ da küçük bir uygulama.

---
+++ nesne_hareket.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define KEY_X 120
+
+int main(){
+char Oyuncu İsmi;
+cout<<"Oyuncu İsmini Giriniz:?";
+cin>>"Oyuncu İsmi";
+
+char key=getch();
+int value= key;
+
+while (value !=KEY_X ) {
+
+}
+
+
+system("pause");
+return 0;
+
+}
